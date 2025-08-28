db55e9da14227d42d9234cef0cfd2db532153a5a
ddumaa <dimaduma62@gmail.com>
2025-08-28 10:33:26 +0300

    ver

--- woo-seedling-limiter.php
+++ woo-seedling-limiter.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce Seedling Quantity Limiter
  * Description: Ограничения на количество товаров из категории: минимум на вариацию и общий минимум по категории.
- * Version: 1.10
+ * Version: 1.1.1
  * Author: Дмитрий Анисимов
  */
 
